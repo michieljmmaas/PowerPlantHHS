4f892c0e03fbc52893c83ea2bdb07693b464259a
syllian <31187238+syllian@users.noreply.github.com>
2019-11-13 11:25:53 +0100

    update plot.py
    Added legend coordinates and made and fixed some bux with calling upon simulator functions.

--- train.py
+++ train.py
@@ -20,14 +20,16 @@ N_FEATURES = N_SOLAR_FEATURES + N_WIND_FEATURES
 
 def train(n_generations, group_size, surface_min, surface_max, angle_min, angle_max, orientation_min, orientation_max,
           model_name=None, load=False, counter=None, directory=None, mutationPercentage=50, target_kw=6000,
-          EnergyArray=None, cost_calculator=None, simulator=None, windturbineType=4, N_WIND_MAX=20, tr_rating=0.12, sp_efficiency=16):
+          EnergyArray=None, cost_calculator=None, simulator=None, windturbineType=4, N_WIND_MAX=100, tr_rating=0.12, sp_efficiency=16):
     """train genetic algorithm"""
     genetic_algorithm = GeneticAlgorith(mutationPercentage, 150, 6, 2, 2, True)
-    cb_cost_table = pd.DataFrame({'area':[1.5, 2.5, 4, 6, 10, 16, 25, 35, 50, 70, 95, 120, 150, 185, 240, 300, 400, 600, 1000,1250, 1600, 2000, 3000, 5000, 8000 , 10000, 12000, 15000, 18000, 22000, 25000, 30000, 40000, 50000],
-                                  'cost':[0.002, 0.003, 0.008, 0.013, 0.014, 0.016, 0.025, 0.035, 0.075, 0.1, 0.15, 0.22, 0.3, 0.39, 0.49, 0.5, 0.62, 0.8, 1.25, 1.6, 2, 2.5, 3.5, 6, 9, 11, 13, 17.5, 20, 30, 40, 50, 60, 72]})
+    cb_cost_table = pd.DataFrame({'area':[1.5, 2.5, 4, 6, 10, 16, 25, 35, 50, 70, 95, 120, 150, 185, 240, 300, 400, 600, 1000,1250, 1600, 2000, 3000, 5000, 
+                                    8000 , 10000, 12000, 15000, 18000, 22000, 25000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 200000],
+                                  'cost':[0.002, 0.003, 0.008, 0.013, 0.014, 0.016, 0.025, 0.035, 0.075, 0.1, 0.15, 0.22, 0.3, 0.39, 0.49, 0.5, 
+                                  0.62, 0.8, 1.25, 1.6, 2, 2.5, 3.5, 6, 9, 11, 13, 17.5, 20, 30, 40, 50, 60, 72, 84, 96, 110, 124, 140, 280]})
     # parameter 2 kosten voor accu per kWh
     if cost_calculator is None:
-        cost_calculator = CostCalculator(190, 400, target_kw, 1000000, cb_cost_table, 1000, 230)
+        cost_calculator = CostCalculator(190, 400, target_kw, 1000000, cb_cost_table, 1000, 100000)
     turbine = Windturbine(windturbineType)
 
     if simulator is None:
@@ -132,13 +134,10 @@ def train(n_generations, group_size, surface_min, surface_max, angle_min, angle_
         saver.save(group_values)
 
 if __name__ == '__main__':
-
-    cb_cost_table = pd.DataFrame({'area': [1.5, 2.5, 4, 6, 10, 16, 25, 35, 50, 70, 95, 120, 150, 185, 240, 300, 400,
-                                           600, 1000, 1250, 1600, 2000, 3000, 5000, 8000, 10000, 12000, 15000, 18000,
-                                           22000, 25000, 30000, 40000, 50000],
-                                  'cost': [0.002, 0.003, 0.008, 0.013, 0.014, 0.016, 0.025, 0.035, 0.075, 0.1, 0.15,
-                                           0.22, 0.3, 0.39, 0.49, 0.5, 0.62, 0.8, 1.25, 1.6, 2, 2.5, 3.5, 6, 9, 11, 13,
-                                           17.5, 20, 30, 40, 50, 60, 72]})
-    cost_calculator = CostCalculator(190, 400, 6000, 1000000, cb_cost_table, 1000, 230)
-    train(100, 100, 10000, 10000000, 0, 90, 0, 359, tr_rating=0.12, cost_calculator=cost_calculator)
+    cb_cost_table = pd.DataFrame({'area':[1.5, 2.5, 4, 6, 10, 16, 25, 35, 50, 70, 95, 120, 150, 185, 240, 300, 400, 600, 1000,1250, 1600, 2000, 3000, 5000, 
+                                    8000 , 10000, 12000, 15000, 18000, 22000, 25000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000, 200000],
+                                  'cost':[0.002, 0.003, 0.008, 0.013, 0.014, 0.016, 0.025, 0.035, 0.075, 0.1, 0.15, 0.22, 0.3, 0.39, 0.49, 0.5, 
+                                  0.62, 0.8, 1.25, 1.6, 2, 2.5, 3.5, 6, 9, 11, 13, 17.5, 20, 30, 40, 50, 60, 72, 84, 96, 110, 124, 140, 280]})
+    cost_calculator = CostCalculator(190, 400, 6000, 1000000, cb_cost_table, 1000, 100000)
+    train(100, 100, 0, 10000000, 0, 90, 0, 359, tr_rating=0.15, cost_calculator=cost_calculator)
 
